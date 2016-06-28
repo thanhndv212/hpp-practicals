738ec148505c2bbc9463b63622174641a4ff84f0
Florent Lamiraux <florent@laas.fr>
2016-06-28 12:45:33 +0200

    Tutorial rrt and manipulation planning.

---
+++ script/manipulation/__init__.py
@@ -0,0 +1,1 @@
+from common import robot, vf, ConstraintGraph, ps, Ground, Box, Pokeball, PathPlayer, gripperName, ballName
---
+++ script/manipulation_box.py
@@ -0,0 +1,36 @@
+from math import sqrt
+from hpp import Transform
+from manipulation import robot, vf, ConstraintGraph, ps, Ground, Box, Pokeball, PathPlayer, gripperName, ballName
+
+vf.loadEnvironmentModel (Ground, 'ground')
+vf.loadEnvironmentModel (Box, 'box')
+vf.moveObstacle ('box/base_link_0', [0.3+0.04, 0, 0.04, 1, 0, 0, 0])
+vf.moveObstacle ('box/base_link_1', [0.3-0.04, 0, 0.04, 1, 0, 0, 0])
+vf.moveObstacle ('box/base_link_2', [0.3, 0.04, 0.04, 1, 0, 0, 0])
+vf.moveObstacle ('box/base_link_3', [0.3, -0.04, 0.04, 1, 0, 0, 0])
+
+vf.loadObjectModel (Pokeball, 'pokeball')
+robot.setJointBounds ('pokeball/base_joint_xyz', [-.4,.4,-.4,.4,-.1,1.])
+
+r = vf.createViewer ()
+
+q1 = [0, -1.57, 1.57, 0, 0, 0, .3, 0, 0.025, 1, 0, 0, 0]
+r (q1)
+
+## Create graph
+graph = ConstraintGraph (robot, 'graph')
+
+
+
+res, q_init, error = graph.applyNodeConstraints ('placement', q1)
+q2 = q1 [::]
+q2 [7] = .2
+
+res, q_goal, error = graph.applyNodeConstraints ('placement', q2)
+
+ps.setInitialConfig (q_init)
+ps.addGoalConfig (q_goal)
+ps.selectPathValidation ("Dichotomy", 0)
+
+pp = PathPlayer (ps.client.basic, r)
+
