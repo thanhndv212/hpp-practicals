9f5fa198b724377c016e99967043463a65488d1a
dvtnguyen <dvtnguyen@laas.fr>
2025-10-31 09:57:38 +0100

    add rrt solution

--- script/motion_planner.py
+++ script/motion_planner.py
@@ -1,42 +1,58 @@
 class MotionPlanner:
-  def __init__ (self, robot, ps):
-    self.robot = robot
-    self.ps = ps
+    def __init__(self, robot, ps):
+        self.robot = robot
+        self.ps = ps
 
-  def solveBiRRT (self, maxIter = float("inf")):
-    print ("Method solveBiRRT is not implemented yet")
-    self.ps.prepareSolveStepByStep ()
-    finished = False
+    def solveBiRRT(self, maxIter=float("inf")):
+        # print("Method solveBiRRT is not implemented yet")
+        self.ps.prepareSolveStepByStep()
+        finished = False
 
-    # In the framework of the course, we restrict ourselves to 2 connected components.
-    nbCC = self.ps.numberConnectedComponents ()
-    if nbCC != 2:
-      raise Exception ("There should be 2 connected components.")
+        # In the framework of the course,
+        # we restrict ourselves to 2 connected components.
+        nbCC = self.ps.numberConnectedComponents()
+        if nbCC != 2:
+            raise Exception("There should be 2 connected components.")
 
-    iter = 0
-    while True:
-      #### RRT begin
-      newConfigs = list ()
+        iter = 0
+        while True:
+            # RRT begin
+            # write your algorithm here
+            # RRT end
+            random_config = self.robot.shootRandomConfig()
 
-      ## Try connecting the new nodes together
-      for i in range (len(newConfigs)):
-        pass
-      #### RRT end
-      ## Check if the problem is solved.
-      nbCC = self.ps.numberConnectedComponents ()
-      if nbCC == 1:
-        # Problem solved
-        finished = True
-        break
-      iter = iter + 1
-      if iter > maxIter:
-        break
-    if finished:
-        self.ps.finishSolveStepByStep ()
-        return self.ps.numberPaths () - 1
+            c1, _ = self.ps.getNearestConfig(random_config, 0)
+            c2, _ = self.ps.getNearestConfig(random_config, 1)
 
-  def solvePRM (self):
-    self.ps.prepareSolveStepByStep ()
-    #### PRM begin
-    #### PRM end
-    self.ps.finishSolveStepByStep ()
+            _, path1, _ = self.ps.directPath(c1, random_config, True)
+            _, path2, _ = self.ps.directPath(c2, random_config, True)
+
+            length1 = self.ps.pathLength(path1)
+            length2 = self.ps.pathLength(path2)
+
+            config1 = self.ps.configAtParam(path1, length1)
+            config2 = self.ps.configAtParam(path2, length2)
+
+            self.ps.addConfigToRoadmap(config1)
+            self.ps.addConfigToRoadmap(config2)
+
+            self.ps.addEdgeToRoadmap(c1, config1, path1, True)
+            self.ps.addEdgeToRoadmap(c2, config2, path2, True)
+            # Check if the problem is solved.
+            nbCC = self.ps.numberConnectedComponents()
+            if nbCC == 1:
+                # Problem solved
+                finished = True
+                break
+            iter = iter + 1
+            if iter > maxIter:
+                break
+        if finished:
+            self.ps.finishSolveStepByStep()
+            return self.ps.numberPaths() - 1
+
+    def solvePRM(self):
+        self.ps.prepareSolveStepByStep()
+        # PRM begin
+        # PRM end
+        self.ps.finishSolveStepByStep()
